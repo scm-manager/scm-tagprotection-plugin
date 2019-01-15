9f2b79608edd805dde5f5e95f1187407e3d913a5
Philipp Czora <pczora@gmail.com>
2019-01-15 16:45:55 +0100

    Make SQ shut up

--- src/main/java/sonia/scm/tagprotection/TagProtectionConfigDto.java
+++ src/main/java/sonia/scm/tagprotection/TagProtectionConfigDto.java
@@ -2,6 +2,7 @@ package sonia.scm.tagprotection;
 
 import de.otto.edison.hal.HalRepresentation;
 import de.otto.edison.hal.Links;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class TagProtectionConfigDto extends HalRepresentation {
 
     private boolean reduceOwnerPrivilege = false;
