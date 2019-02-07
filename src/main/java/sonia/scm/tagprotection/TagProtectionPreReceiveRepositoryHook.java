c7aadf1fe3f6400b80f1a8dd42c33fcfe437d573
Mohamed Karray <mohamed.karray.sr@gmail.com>
2019-02-07 12:03:20 +0100

    bugfix verify tag support

--- src/main/java/sonia/scm/tagprotection/TagProtectionPreReceiveRepositoryHook.java
+++ src/main/java/sonia/scm/tagprotection/TagProtectionPreReceiveRepositoryHook.java
@@ -11,6 +11,7 @@ import sonia.scm.plugin.Extension;
 import sonia.scm.repository.PreReceiveRepositoryHookEvent;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.Tag;
+import sonia.scm.repository.api.HookFeature;
 import sonia.scm.user.User;
 
 import java.util.List;
@@ -20,41 +21,45 @@ import java.util.List;
 @EagerSingleton
 public class TagProtectionPreReceiveRepositoryHook {
 
-    private static final Logger logger = LoggerFactory.getLogger(TagProtectionPreReceiveRepositoryHook.class);
+  private static final Logger logger = LoggerFactory.getLogger(TagProtectionPreReceiveRepositoryHook.class);
 
-    private final TagProtectionValidator tagProtectionValidator;
+  private final TagProtectionValidator tagProtectionValidator;
 
-    @Inject
-    public TagProtectionPreReceiveRepositoryHook(TagProtectionValidator tagProtectionValidator) {
+  @Inject
+  public TagProtectionPreReceiveRepositoryHook(TagProtectionValidator tagProtectionValidator) {
 
-        this.tagProtectionValidator = tagProtectionValidator;
+    this.tagProtectionValidator = tagProtectionValidator;
+  }
+
+  @Subscribe(async = false)
+  public void onEvent(PreReceiveRepositoryHookEvent event) {
+
+    Repository repository = event.getRepository();
+    if (repository != null) {
+
+      Subject subject = SecurityUtils.getSubject();
+      if (!event.getContext().isFeatureSupported(HookFeature.TAG_PROVIDER)) {
+        return;
+      }
+
+      List<Tag> deletedTags = event.getContext().getTagProvider().getDeletedTags();
+
+      boolean tagsMustBeProtected = tagProtectionValidator.tagsMustBeProtected(repository, deletedTags);
+      if (tagsMustBeProtected) {
+
+        User user = subject.getPrincipals().oneByType(User.class);
+        String message = String.format("Deleting tags not allowed for user %s in repository %s/%s", user.getName(), repository.getNamespace(), repository.getName());
+        logger.info(message);
+        event.getContext().getMessageProvider().sendMessage(message);
+        throw new TagProtectionException(message);
+      } else {
+
+        logger.trace("Tag Protection does not need to be enforced.");
+      }
+    } else {
+      logger.warn("received hook without repository");
+
     }
-
-    @Subscribe(async = false)
-    public void onEvent(PreReceiveRepositoryHookEvent event) {
-
-        Repository repository = event.getRepository();
-        if (repository != null) {
-
-            Subject subject = SecurityUtils.getSubject();
-            List<Tag> deletedTags = event.getContext().getTagProvider().getDeletedTags();
-
-            boolean tagsMustBeProtected = tagProtectionValidator.tagsMustBeProtected(repository, deletedTags);
-            if (tagsMustBeProtected) {
-
-                User user = subject.getPrincipals().oneByType(User.class);
-                String message = String.format("Deleting tags not allowed for user %s in repository %s/%s", user.getName(), repository.getNamespace(), repository.getName());
-                logger.info(message);
-                event.getContext().getMessageProvider().sendMessage(message);
-                throw new TagProtectionException(message);
-            } else {
-
-                logger.trace("Tag Protection does not need to be enforced.");
-            }
-        } else {
-            logger.warn("received hook without repository");
-
-        }
-    }
+  }
 
 }
